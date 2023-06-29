1e21a4469b8900c734795cc5efc216bfac4ec30c
Shantanu9421 <110804946+Shantanu9421@users.noreply.github.com>
2023-06-29 18:44:02 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,87 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from 'react';
+
+function App() {
+  const [rate, setcount] = useState({ Good: 0, Neutral: 0, Bad: 0 , All:0})
+  const all = ()=>{
+    setcount(preCount=>({...preCount, All: preCount.All + 1 }))
+  }
+  const good = () => {
+    setcount(preCount => ({ ...preCount, Good: preCount.Good + 1 }))
+    all()
+  }
+  const neutral = () => {
+    setcount(preCount => ({ ...preCount, Neutral: preCount.Neutral + 1 }))
+    all()
+  }
+  const bad = () => {
+    setcount(preCount => ({ ...preCount, Bad: preCount.Bad + 1 }))
+    all()
+  }
+  const calculateAverage = () => {
+    const totalReviews = rate.All;
+    const totalScore = rate.Good - rate.Bad;
+    if (totalReviews == 0) {
+      return 0;
+    }
+    return totalScore / totalReviews;
+  };
+  
+
+  const calculatePositivePercentage = () => {
+    const totalReviews = rate.All;
+    const positiveReviews = rate.Good;
+    if (totalReviews == 0) {
+      return 0;
+    }
+    return (positiveReviews / totalReviews) * 100;
+  };
+
+
+  const average = calculateAverage()
+  const positive =calculatePositivePercentage()
+  return (
+    <div>
+      
+
+      <p>
+        FEEDBACK SYSTEM
+      </p>
+      <button onClick={good}>Good</button>
+      <br></br>
+      <button onClick={neutral}>Neutral</button>
+      <br></br>
+      <button onClick={bad}>Bad</button>
+      <br></br>
+      <p>
+        STATISTICS
+      </p>
+      <p>
+        Good: {rate.Good}
+      </p>
+
+      <p>
+        Neutral: {rate.Neutral}
+      </p>
+
+      <p>
+        Bad: {rate.Bad}
+      </p>
+
+      <p>
+        All: {rate.All}
+      </p>
+      <p>
+        Average:{average}
+      </p>
+      
+      <p>
+        Positive:{positive}
+      </p>
+
+    </div>
+  );
+}
+
+export default App;
